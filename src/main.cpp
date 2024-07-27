957d2cc0c15f4152cfa67c4c6f174b0cdc3edc4a
Rate980 <xv0nnj0@icloud.com>
2024-07-28 02:20:51 +0900

    delete 仮のやつ消した

--- src/main.cpp
+++ src/main.cpp
@@ -8,12 +8,9 @@
 #define MOTOR_PIN 22
 Servo futa;
 
-void motorTask(void *);
-
 void setup()
 {
   Serial.begin(115200);
-  Serial2.begin(115200);
 
   auto cfg = M5.config();
 
@@ -25,19 +22,7 @@ void setup()
   M5.Lcd.drawPngFile(SPIFFS, "/mayo.png", 0, 0);
   futa.attach(SERVO_PIN, 500, 2400);
 
-  xTaskCreatePinnedToCore(
-      motorTask,   /* Task function. */
-      "motorTask", /* name of task. */
-      1024,        /* Stack size of task */
-      NULL,        /* parameter of the task */
-      1,           /* priority of the task */
-      NULL,        /* Task handle to keep track of created task */
-      0);          /* pin task to core 0 */
-
-  // ledcSetup(0, 50, 8);
-  // ledcAttachPin(SERVO_PIN, 0);
-
-  while (!Serial || !Serial2)
+  while (!Serial)
   {
     delay(1);
   }
@@ -53,18 +38,18 @@ void loop()
   delay(1000);
 }
 
-void motorTask(void *)
-{
-  int motorState = LOW;
-  while (1)
-  {
-    M5.update();
-    if (M5.BtnA.wasPressed())
-    {
-      Serial.println("Button A was pressed");
-      motorState = !motorState;
-      digitalWrite(MOTOR_PIN, motorState);
-    }
-    delay(100);
-  }
-}
+// void motorTask(void *)
+// {
+//   int motorState = LOW;
+//   while (1)
+//   {
+//     M5.update();
+//     if (M5.BtnA.wasPressed())
+//     {
+//       Serial.println("Button A was pressed");
+//       motorState = !motorState;
+//       digitalWrite(MOTOR_PIN, motorState);
+//     }
+//     delay(100);
+//   }
+// }
