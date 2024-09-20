7f41fb61b63c672ffb519117cbef4edf4e722fda
Rate980 <2220208@ecc.ac.jp>
2024-09-20 17:20:30 +0900

    fix

--- src/main.cpp
+++ src/main.cpp
@@ -7,13 +7,16 @@
 //
 #define SERVO_PIN 21
 #define MOTOR_PIN 22
-#define NEOPIXEL_PIN 16
+#define NEOPIXEL_PIN 17
 #define NEOPIXEL_NUM 10
 
 Servo futa;
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(NEOPIXEL_NUM, NEOPIXEL_PIN, NEO_GRB + NEO_KHZ800);
 TaskHandle_t cleadTaskHandle = NULL;
 void cleanTask(void *);
+void motorTask(void *);
+void openServo();
+void closeServo();
 
 void setup()
 {
@@ -22,28 +25,37 @@ void setup()
   auto cfg = M5.config();
 
   M5.begin(cfg);
-  SPIFFS.begin(true);
   pinMode(SERVO_PIN, OUTPUT);
   pinMode(MOTOR_PIN, OUTPUT);
+  digitalWrite(MOTOR_PIN, LOW);
 
   futa.attach(SERVO_PIN, 500, 2400);
 
   strip.begin();
+  closeServo();
 
   while (!Serial)
   {
     delay(1);
   }
-  M5.Lcd.drawPngFile(SPIFFS, "/default.png");
+  if (!SPIFFS.begin())
+  {
+    Serial.println("SPIFFS Mount Failed");
+    return;
+  }
+
+  M5.Lcd.drawPngFile(SPIFFS, "/default.png", 0, 0);
 }
 
 void onSerialAvailable()
 {
   String inp = Serial.readStringUntil('\n');
+  Serial.println(inp);
 
   if (inp.charAt(0) == '0') // on_unko
   {
     Serial.println("on_unko");
+    openServo();
     if (cleadTaskHandle != NULL && eTaskGetState(cleadTaskHandle) != eDeleted)
     {
       vTaskDelete(cleadTaskHandle);
@@ -52,8 +64,17 @@ void onSerialAvailable()
   }
   if (inp.charAt(0) == '1') // on_clean
   {
-    xTaskCreatePinnedToCore(cleanTask, "cleanTask", 4096, NULL, 1, &cleadTaskHandle, 0);
-    Serial.println("on_clean");
+    M5.Lcd.drawPngFile(SPIFFS, "/nagare.png");
+  }
+  if (inp.charAt(0) == '2') // on_clean_done
+  {
+    closeServo();
+    M5.Lcd.drawPngFile(SPIFFS, "/default.png");
+    xTaskCreatePinnedToCore(motorTask, "motorTask", 4096, NULL, 1, NULL, 0);
+  }
+  if (inp.charAt(0) == '3') // reset
+  {
+    M5.Lcd.drawPngFile(SPIFFS, "/default.png");
   }
 }
 
@@ -67,10 +88,31 @@ void loop()
   delay(1);
 }
 
+void motorTask(void *)
+{
+  delay(1000);
+  digitalWrite(MOTOR_PIN, HIGH);
+  delay(1000);
+  digitalWrite(MOTOR_PIN, LOW);
+  vTaskDelete(NULL);
+}
+
 void cleanTask(void *)
 {
   M5.Lcd.drawPngFile(SPIFFS, "/nagare.png");
   delay(1000);
+  closeServo();
   M5.Lcd.drawPngFile(SPIFFS, "/default.png");
+  xTaskCreatePinnedToCore(motorTask, "motorTask", 4096, NULL, 1, NULL, 0);
   vTaskDelete(NULL);
+}
+
+void openServo()
+{
+  futa.write(0);
+}
+
+void closeServo()
+{
+  futa.write(180);
 }
